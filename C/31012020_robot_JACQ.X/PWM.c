ef42dc4ac9b534b9042d1d8acda130ac3f80789e
IUT GEII Toulon E 105 Table 4 <jules.prince.fr@icloud.com>
2020-12-11 09:58:36 +0100

    ??
    va se faire revert anyways

--- C/31012020_robot_JACQ.X/PWM.c
+++ C/31012020_robot_JACQ.X/PWM.c
@@ -7,9 +7,6 @@
 #include "etats.h"
 
 #define PWMPER 40.0
-#define COEFF_VITESSE_LINEAIRE_PERCENT 1/25.
-#define COEFF_VITESSE_ANGULAIRE_PERCENT 1/50.
-
 unsigned char accelerationGauche = 10;
 unsigned char accelerationDroite = 10;
 unsigned char freinDroite = 7;
@@ -94,10 +91,3 @@ void PWMSetSpeedConsigne (float vitesseEnPourcents, char moteur)
     else if (moteur == MOTEUR_GAUCHE)
         robotState.vitesseGaucheConsigne = vitesseEnPourcents;
 }
-void PWMSetSpeedConsignePolaire() {
-// CorrectionAngulaire
-double erreurVitesseAngulaire =  ;
-double sortieCorrectionLineaire= ;
-double correctionVitesseAngulaire=;
-double correctionVitesseAngulairePourcent =  correctionVitesseAngulaire*COEFF_VITESSE_ANGULAIRE_PERCENT 1/50.;
-
